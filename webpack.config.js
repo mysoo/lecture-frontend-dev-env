c1f4c391ce0aefa46b3e5a13cee586a67a5cd77e
mysoo <myungsoocc@gmail.com>
2024-08-29 15:05:47 +0900

    feat: webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,37 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      //   {
+      //     test: /\.(png|jpg|gif|svg)$/,
+      //     loader: "file-loader",
+      //     options: {
+      //       publicPath: "./dist",
+      //       name: "[name].[ext]?[hash]",
+      //     },
+      //   },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist",
+          name: "[name].[ext]?[hash]",
+          limit: 10000, //10kb
+        },
+      },
+    ],
+  },
+};
